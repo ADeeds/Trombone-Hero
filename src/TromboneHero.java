1d17ce323307dbeae216e6a831f03bd4710a6636
ADeeds <deeds@usc.edu>
2014-03-08 23:05:59 -0800

    Init

---
+++ src/TromboneHero.java
@@ -0,0 +1,15 @@
+
+public class TromboneHero {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		new TromboneHero();
+	}
+	
+	TromboneHero() {
+		System.out.println("TROMBONE HERO!!!!!!!!");
+	}
+
+}
