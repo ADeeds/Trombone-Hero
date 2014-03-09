1c0c2f2db3ea050e3b8bebcf3761f37f15f84bf2
Chase Wagoner <cadwagoner@gmail.com>
2014-03-09 06:27:48 -0700

    Added skeleton for Judge

---
+++ src/stats/Judge.java
@@ -0,0 +1,21 @@
+package stats;
+
+import gui.Board;
+import music.*;
+
+public class Judge {
+
+	private int boneage;
+	private int score;
+	private Board board;
+	
+	public Judge(Board board) {
+		this.board = board;
+		boneage = 50;
+		
+	}
+
+	public void assess(Note n) {
+		
+	}
+}
