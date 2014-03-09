5e7cbbd31739e1ba8fcd25e943fb433922ff6831
Chase Wagoner <cadwagoner@gmail.com>
2014-03-09 03:46:15 -0700

    Mapped positions to distances

--- src/music/Note.java
+++ src/music/Note.java
@@ -2,30 +2,41 @@ package music;
 
 /** Note represents a single instance of a pitch being played */
 public class Note {
-	static Position noteToPos(String name) {
-		if (name.equals("Bb")) return Position.FIRST;
-		if (name.equals("B")) return Position.SEVENTH;
-		if (name.equals("C")) return Position.SIXTH;
-		if (name.equals("C#") || name.equals("Db")) return Position.FIFTH;
-		if (name.equals("D")) return Position.FOURTH;
-		if (name.equals("Eb") || name.equals("D#")) return Position.THIRD;
-		if (name.equals("E") || name.equals("Fb")) return Position.SECOND;
-		if (name.equals("F")) return Position.FIRST;
-		if (name.equals("Gb") || name.equals("F#")) return Position.FIFTH;
-		if (name.equals("G")) return Position.FOURTH;
-		if (name.equals("Ab") || name.equals("G#")) return Position.THIRD;
-		if (name.equals("A")) return Position.SECOND;
+	
+	final byte[] PositionDistances =
+		{	00, /* Zeroth - Ignore */
+			34, /* First */
+			43, /* Second */
+			52, /* Third */
+			31, /* Fourth */
+			74, /* Fifth */
+			82, /* Sixth */
+			95  /* Seventh */
+		};
+	
+	static int noteToPos(String name) {
+		if (name.equals("Bb")) return 1;
+		if (name.equals("B")) return 7;
+		if (name.equals("C")) return 6;
+		if (name.equals("C#") || name.equals("Db")) return 5;
+		if (name.equals("D")) return 4;
+		if (name.equals("Eb") || name.equals("D#")) return 3;
+		if (name.equals("E") || name.equals("Fb")) return 2;
+		if (name.equals("F")) return 1;
+		if (name.equals("Gb") || name.equals("F#")) return 5;
+		if (name.equals("G")) return 4;
+		if (name.equals("Ab") || name.equals("G#")) return 3;
+		if (name.equals("A")) return 2;
 		
 		System.err.println("UNKNOWN NOTE:" + name);
-		return Position.FIRST;
+		return 1;
 	}
 
 	/** The name of the note pitch (on trombone, same as concert pitch) */
 	public String name;
 	
 	/** Slide position on trombone */
-	public enum Position { FIRST, SECOND, THIRD, FOURTH, FIFTH, SIXTH, SEVENTH }
-	public Position position;
+	public int position;
 	
 	/** Octave number. Defaults to 1 */
 	public int octave;
@@ -36,20 +47,24 @@ public class Note {
 	/** The length of the note in beats */
 	public float duration;
 
-	private void set(String n, Position pos, float start, float dur) {
+	private void set(String n, float start, float dur) {
 		this.name = n;
-		this.position = pos;
+		this.position = noteToPos(n);
 		this.startBeat = start;
 		this.duration = dur;
 	}
 	
-	Note(String n, Position pos, float start, float dur) {
-		set(n, pos, start, dur);
+	public Note(String n, float start, float dur) {
+		set(n, start, dur);
 		this.octave = 1;
 	}
 
-	Note(String n, Position pos, int oct, float start, float dur) {
-		set(n, pos, start, dur);
+	public Note(String n, int oct, float start, float dur) {
+		set(n, start, dur);
 		this.octave = oct;
+	}
+	
+	public int getPositionDistance() {
+		return PositionDistances[this.position];
 	}
 }
