5ed4ce07db768ade8e41a30994077a76a28707e1
Chase Wagoner <cadwagoner@gmail.com>
2014-03-08 23:48:17 -0800

    Slight change

--- src/music/Note.java
+++ src/music/Note.java
@@ -6,18 +6,22 @@ package music;
  */
 public class Note {
 
+
+	/** The name of the note pitch (on trombone, same as concert pitch) */
+	public String name;
+	
 	/** Slide position on trombone */
 	public enum Position { FIRST, SECOND, THIRD, FOURTH, FIFTH, SIXTH, SEVENTH }
 	public Position position;
+	
+	/** Octave number. Defaults to 1 */
+	public int octave;
 	
 	/** The starting beat of the note */
 	public float startTime;
 	
 	/** The length of the note in beats */
 	public float duration;
-	
-	/** The name of the note pitch (on trombone, same as concert pitch) */
-	public String name;
 
 	Note(Position pos, float start, float dur) {
 		this.position = pos;
