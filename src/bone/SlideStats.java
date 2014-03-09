208002bcf83da3ed749648b7aa8758f6ab0f3630
ADeeds <deeds@usc.edu>
2014-03-09 08:37:11 -0700

    Fixed boneage

--- src/bone/SlideStats.java
+++ src/bone/SlideStats.java
@@ -33,8 +33,8 @@ public class SlideStats {
 	}
 	
 	public static int getFirstOffset(int centimeters) {
-		int cur = getPositionDistance(1, centimeters);
-		if (cur < 2) cur = 0;
+		int cur = centimeters - 34;
+		if (cur < 3) cur = 0;
 		return cur;
 	}
 }
